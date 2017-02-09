974fed25153fd5cea0a768b82015db768924c15a
Simon Fridlund <simon@fridlund.email>
2017-02-09 14:46:41 +0100

    Add package files

---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  documentation: 'README.md',
+  git: 'git+https://github.com/meteor-collectionbehaviours/softremove',
+  name: 'collectionbehaviours:softremove',
+  summary: 'Soft remove documents in your collections',
+  version: '1.0.0-alpha.1',
+});
+
+Package.onUse(function onUse(api) {
+  api.versionsFrom('1.4.2.2');
+
+  api.use([
+    'check',
+    'ecmascript',
+    'matb33:collection-hooks@0.8.4',
+    'mongo',
+  ]);
+
+  api.mainModule('behaviour.js', ['client', 'server'], { lazy: true });
+});
